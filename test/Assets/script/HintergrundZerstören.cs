6a5978897821d467cdaa06a0070a5c576b2a90c2
magharmi <39556577+magharmi@users.noreply.github.com>
2018-09-03 19:06:02 +0200

    Parallaxing fix

---
+++ test/Assets/script/HintergrundZerstören.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintergrundZerstören : MonoBehaviour {
+
+    public GameObject[] hintergründe;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "spieler")
+        {
+            Debug.Log("SPIELER ERKANNT");
+            for(int i = 0; i < hintergründe.Length; i++)
+            {
+                Destroy(hintergründe[i]);
+            }
+        }
+    }
+}
