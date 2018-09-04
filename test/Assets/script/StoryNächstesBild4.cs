fd956ed3c189e65fd0b9ff9d01f94da51f455770
magharmi <39556577+magharmi@users.noreply.github.com>
2018-09-04 20:46:23 +0200

    Storybilder Scene

---
+++ test/Assets/script/StoryNächstesBild4.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StoryNächstesBild4 : MonoBehaviour
+{
+
+    public void NächstesBild()
+    {
+        SceneManager.LoadScene("test");
+    }
+}
