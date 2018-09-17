17c75beb272607e585742f9f38a0c9cac572d4cf
Goregg <39563843+Goregg@users.noreply.github.com>
2018-09-17 17:01:17 +0200

    Storiebilder fertig

---
+++ test/Assets/script/StoryNächstesBildKopie.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StoryNächstesBildKopie : MonoBehaviour {
+
+    private int bildNummer = 0;
+    GameObject[] bilder;  
+
+    void Start()
+    {    
+    }
+
+    public void NächstesBild()
+    {
+        if(bildNummer == 0){
+            bilder = GameObject.FindGameObjectsWithTag("Storiebild");
+            Debug.Log("Erster Klick");
+            Debug.Log("Anzahl der Bilder: " + bilder.Length );
+            foreach(GameObject r in bilder){
+                Debug.Log(r.gameObject); 
+            }
+        }
+        Debug.Log("Klick " + bildNummer);
+        bildNummer++;
+        if(bildNummer < bilder.Length){
+            Destroy(GameObject.Find("00" + bildNummer));
+        }
+        else{
+            SceneManager.LoadScene("test");
+        }
+    }
+}
