5a65aa2d5fa0ac37e1b72f6335293b21d5667d1f
magharmi <39556577+magharmi@users.noreply.github.com>
2018-09-19 00:36:33 +0200

    Mumien spawnen

--- test/Assets/script/AnubisKampf.cs
+++ test/Assets/script/AnubisKampf.cs
@@ -8,10 +8,12 @@ public class AnubisKampf : MonoBehaviour {
     private Vector3 startPosition;
     private bool anubisZurück = false;
     private bool mumienSpawnen = false;
+    private GameObject[] mumien;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         anubis = GameObject.FindGameObjectWithTag("Boss");
+        mumien = GameObject.FindGameObjectsWithTag("AnubisMumien");
         startPosition = anubis.transform.position;
 }
 	
@@ -21,19 +23,21 @@ public class AnubisKampf : MonoBehaviour {
         {
             if (anubisZurück == false)
             {
+                anubis.GetComponent<BoxCollider2D>().enabled = false;
                 anubis.GetComponent<GegnerAI>().speed = 0;
                 anubis.transform.position = Vector2.MoveTowards(anubis.transform.position, startPosition, 7 * Time.deltaTime);
-                anubis.GetComponent<GegnerAI>().leben = 350;
                 if (anubis.transform.position == startPosition)
                 {
                     Debug.Log("Anubis zurück");
                     anubisZurück = true;
                     mumienSpawnen = true;
+                    anubis.GetComponent<GegnerAI>().leben = 350;
                 }
             }
             else if (mumienSpawnen == true)
             {
-                //MUMIEN SPAWNEN FUNKTION!!!
+                mumienSpawnenFunc();
+                Debug.Log("Mumien spawnen");
                 mumienSpawnen = false;
             }
         }
@@ -44,6 +48,25 @@ public class AnubisKampf : MonoBehaviour {
         anubis.GetComponent<GegnerAI>().speed = 5;
     }
 
-    
+    public void mumienSpawnenFunc()
+    {
+        StartCoroutine(warten(mumien.Length-1));
+    }
+
+    IEnumerator warten(int mumienNummer)
+    {
+        mumien[mumienNummer].GetComponent<SpriteRenderer>().enabled = true;
+        mumien[mumienNummer].GetComponent<GegnerAI>().enabled = true;
+        yield return new WaitForSeconds(2);
+        if(mumienNummer == 8)
+        {
+            StartCoroutine(warten(mumienNummer - 1));
+            StartCoroutine(warten(mumienNummer - 2));
+        }
+        else if(mumienNummer > 0)
+        {
+            StartCoroutine(warten(mumienNummer - 1));
+        }
+    }
 
 }
