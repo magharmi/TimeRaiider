fabbd4912aabd9c3a55115d29a702b6cc1b702e3
magharmi <39556577+magharmi@users.noreply.github.com>
2018-09-28 15:30:14 +0200

    Sklaven eingefügt, kleiner Bosskampf reingepackt

---
+++ test/Assets/script/BoxkampfTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxkampfTrigger : MonoBehaviour {
+
+
+    private GameObject MainCamera, BosskampfKamera, BosskampfWand, Landwirt;
+
+	// Use this for initialization
+	void Start () {
+        MainCamera = GameObject.Find("Main Camera");
+        BosskampfKamera = GameObject.Find("Bosskampf Kamera");
+        BosskampfWand = GameObject.Find("Bosskampf Wand");
+        Landwirt = GameObject.Find("Landwirt (1)");
+	}
+	
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.tag == "spieler")
+        {
+            Landwirt.GetComponent<SpriteRenderer>().enabled = true;
+            MainCamera.SetActive(false);
+            BosskampfKamera.GetComponent<Camera>().enabled = true;
+            BosskampfWand.GetComponent<BoxCollider2D>().enabled = true;
+            Destroy(gameObject);
+        }
+    }
+}
