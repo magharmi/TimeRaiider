aa32ca179b525f23e1b6bd2d839af6e2afcc04ab
magharmi <39556577+magharmi@users.noreply.github.com>
2018-10-18 16:06:47 +0200

    Kamera nach Bosskampf wieder normal.

---
+++ test/Assets/script/LandwirtTot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandwirtTot : MonoBehaviour
+{
+
+
+    private GameObject MainCamera, BosskampfKamera, BosskampfWandRechts;
+
+    // Use this for initialization
+    void Start()
+    {
+        MainCamera = GameObject.Find("Main Camera");
+        BosskampfKamera = GameObject.Find("Bosskampf Kamera");
+        BosskampfWandRechts = GameObject.Find("Bosskampf Wand rechts");
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (gameObject.GetComponent<GegnerAI>().leben <= 0)
+        {
+            MainCamera.SetActive(true);
+            Destroy(BosskampfKamera);
+            Destroy(BosskampfWandRechts);
+        }
+    }
+}
