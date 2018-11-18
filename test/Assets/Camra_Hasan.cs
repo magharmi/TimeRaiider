6b824bebe201c6a59a16504128afd5bdcc175fe4
Hasan <turanpanzer@hotmail.de>
2018-11-18 23:50:55 +0100

    Testet_sicherheits_Kopie
    Habe spieler hizugefügt guck mal ob es funktioniert?

---
+++ test/Assets/Camra_Hasan.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Camra_Hasan : MonoBehaviour
+{
+
+    public Vector3 cameraPosition = Vector3.zero;
+    public Transform target;
+
+    void FixedUpdate()
+    {
+
+        cameraPosition = new Vector3(
+            Mathf.SmoothStep(transform.position.x, target.transform.position.x, 0.3f),
+            Mathf.SmoothStep(transform.position.y, target.transform.position.y, 0.3f));
+
+    }
+
+    void LateUpdate()
+    {
+
+        transform.position = cameraPosition + Vector3.forward * -10;
+
+    }
+
+}
