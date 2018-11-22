3cf20481fc5321e7a184b10ed7a3175acd911222
magharmi <39556577+magharmi@users.noreply.github.com>
2018-11-22 14:42:41 +0100

    Revert "Update_Sicherheit_Kopie"
    This reverts commit c935ccb253e2e591b761edc51bc8b2a5ff2b0895.

--- test/Assets/script/lvlmanager.cs
+++ test/Assets/script/lvlmanager.cs
@@ -16,8 +16,7 @@ public class lvlmanager : MonoBehaviour {
     public Text timertxt;
     public float roundTimer;
     private DialogueManager dialogueManager;
-    [SerializeField]
-    public Button shop;
+
 
 
     void Start()
