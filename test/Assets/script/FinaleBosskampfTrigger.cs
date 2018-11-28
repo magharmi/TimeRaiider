b262bf56db76dbc7619a1361487b709dd435a5b2
magharmi <39556577+magharmi@users.noreply.github.com>
2018-11-28 14:31:09 +0100

    Bosskampf Vorbereitung

---
+++ test/Assets/script/FinaleBosskampfTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinaleBosskampfTrigger : MonoBehaviour
+{
+
+
+    private GameObject MainCamera, BosskampfKamera, BosskampfWand;
+
+    // Use this for initialization
+    void Start()
+    {
+        MainCamera = GameObject.Find("Main Camera");
+        BosskampfKamera = GameObject.Find("Bosskampf Kamera");
+        BosskampfWand = GameObject.Find("Bosskampf Wand");
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "spieler")
+        {
+            MainCamera.SetActive(false);
+            BosskampfKamera.GetComponent<Camera>().enabled = true;
+            BosskampfWand.GetComponent<BoxCollider2D>().enabled = true;
+            Destroy(gameObject);
+        }
+    }
+}
