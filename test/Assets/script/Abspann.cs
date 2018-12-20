61e73dc55b3652b24331b01001659cb7dcd91b80
Goregg <39563843+Goregg@users.noreply.github.com>
2018-12-20 02:51:06 +0100

    Abspann

---
+++ test/Assets/script/Abspann.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Abspann : MonoBehaviour {
+
+    private int bildNummer = 0;
+    GameObject[] bilder;  
+
+    void Start()
+    {    
+    }
+
+    public void NächstesBild()
+    {
+        if(bildNummer == 0){
+            bilder = GameObject.FindGameObjectsWithTag("Storiebild");
+            Debug.Log("Erster Klick");
+            Debug.Log("Anzahl der Bilder: " + bilder.Length );
+            foreach(GameObject r in bilder){
+                Debug.Log(r.gameObject); 
+            }
+        }
+        Debug.Log("Klick " + bildNummer);
+        bildNummer++;
+        if(bildNummer < bilder.Length - 1){
+            Destroy(GameObject.Find("00" + bildNummer));
+        }
+        else
+        {
+            Destroy(GameObject.Find("00" + bildNummer));
+            Destroy(GameObject.Find("1"));
+            Destroy(GameObject.Find("Text"));
+        }
+    }
+}
