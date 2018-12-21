86000329bcf98cc531d408476a3e101bcc4def90
magharmi <39556577+magharmi@users.noreply.github.com>
2018-12-21 20:46:47 +0100

    Opa verfolgt nicht mehr, Kakteen machen Schaden

---
+++ test/Assets/script/OpaVerfolgtNichtMehr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpaVerfolgtNichtMehr : MonoBehaviour {
+
+    private GameObject opa;
+
+	// Use this for initialization
+	void Start () {
+        opa = GameObject.Find("Grossvater");
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        opa.GetComponent<GegnerAI>().enabled = false;
+    }
+}
