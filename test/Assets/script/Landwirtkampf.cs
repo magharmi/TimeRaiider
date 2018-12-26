318a8ce80f2ff3414a901e08affe143b29b3cf38
magharmi <39556577+magharmi@users.noreply.github.com>
2018-12-26 16:52:34 +0100

    Landwirtkampf Fix

---
+++ test/Assets/script/Landwirtkampf.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landwirtkampf : MonoBehaviour
+{
+    private GameObject landwirt;
+
+    // Use this for initialization
+    void Start()
+    {
+        landwirt = GameObject.Find("Landwirt (1)");
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "spieler")
+        {
+            landwirt.GetComponent<GegnerAI>().enabled = true;
+        }
+    }
+}
