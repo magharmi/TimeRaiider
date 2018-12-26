d498baee77acbaff2cd516886e9418b34780c615
magharmi <39556577+magharmi@users.noreply.github.com>
2018-12-26 18:21:24 +0100

    Höhle Item um Steine zu zerstören

---
+++ test/Assets/script/SteineZerstoerbar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteineZerstoerbar : MonoBehaviour {
+    public GameObject[] steine;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "spieler")
+        {
+            for(int i = 0; i < steine.Length; i++)
+            {
+                steine[i].GetComponent<Gegner>().enabled = true;
+            }
+        }
+    }
+}
