e66c31f5cce8c4bf10bdcc63feb292fcbe2d3e40
magharmi <39556577+magharmi@users.noreply.github.com>
2018-12-29 18:27:24 +0100

    Laserkanonen schiessen

---
+++ test/Assets/script/LaserkanoneSchiesst.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserkanoneSchiesst : MonoBehaviour
+{
+
+    //spawn finden
+    public Transform bulletspawn;
+    public Rigidbody2D bulletPrefab;
+    public float bulletSpeed = 750;
+    public float schussGeschwindigkeit;
+    public float radius = 20;
+
+    private Transform player;
+    private Rigidbody2D clone;
+    private bool geschossen;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("spieler").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        while (geschossen == false)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        geschossen = true;
+        yield return new WaitForSeconds(schussGeschwindigkeit);
+        if (Vector3.Distance(player.transform.position, bulletspawn.transform.position) < 30)
+        {
+
+            Vector3 difference = player.position - bulletspawn.position;
+            float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            Debug.Log("test");
+            if(rotationZ < -80)
+            {
+                bulletspawn.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+                clone = Instantiate(bulletPrefab, bulletspawn.position, bulletspawn.rotation);
+
+                clone.AddForce(bulletspawn.transform.right * bulletSpeed);
+            }
+        }
+        geschossen = false;
+    }
+}
