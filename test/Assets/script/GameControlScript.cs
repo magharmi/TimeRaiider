43c6adcabbcdf36690b6429dae4d6b8e9a4adb84
Hasan <turanpanzer@hotmail.de>
2018-12-30 01:45:29 +0100

    -Akt1_Shop_System
    -Shop_Scene
    -Gold aufsammeln
    -WaffeKaufen

---
+++ test/Assets/script/GameControlScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControlScript : MonoBehaviour {
+
+	public Text moneyText;
+	public static int moneyAmount;
+	int isRifleSold;
+	public GameObject rifle;
+
+	// Use this for initialization
+	void Start () {
+		moneyAmount = PlayerPrefs.GetInt ("MoneyAmount");
+		isRifleSold = PlayerPrefs.GetInt ("IsRifleSold");
+
+		if (isRifleSold == 1)
+			rifle.SetActive (true);
+		else
+			rifle.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moneyText.text = "Gold: " + moneyAmount.ToString() + "$";
+	}
+
+	public void gotoShop()
+	{
+		PlayerPrefs.SetInt ("MoneyAmount", moneyAmount);
+		SceneManager.LoadScene ("ShopStoneAge");
+	}
+}
