92873ccbfb70c072f1ce08914064915999d04457
magharmi <39556577+magharmi@users.noreply.github.com>
2019-01-05 14:24:38 +0100

    Ziel Scripte speichern EP, Level und Gold. Spielerleben und EP Anzeige überall eingefügt

--- test/Assets/script/ZielItemNächsterAkt.cs
+++ test/Assets/script/ZielItemNächsterAkt.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ZielItemNächsterAkt : MonoBehaviour {
     public string nächstesLevel;
+    private static int moneyAmount;
+    private Slider sl;
+    private Text levelAnzeige;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "spieler")
         {
+            PlayerPrefs.SetInt("MoneyAmount", moneyAmount);
+            PlayerPrefs.SetFloat("EPValue", sl.value);
+            PlayerPrefs.SetInt("SpielerLevel", int.Parse(levelAnzeige.text));
             SceneManager.LoadScene(nächstesLevel);
         }
     }
--- test/Assets/script/ziel.cs
+++ test/Assets/script/ziel.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ziel : MonoBehaviour
 {
     public string level;
     public int LevelToUnlock;
+    private static int moneyAmount;
+    private Slider sl;
+    private Text levelAnzeige;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,6 +19,9 @@ public class ziel : MonoBehaviour
             Debug.Log("ziel erreicht");
             PlayerPrefs.SetInt("levelReached", LevelToUnlock);
             PlayerPrefs.SetString("letzteScene", level);
+            PlayerPrefs.SetInt("MoneyAmount", moneyAmount);
+            PlayerPrefs.SetFloat("EPValue", sl.value);
+            PlayerPrefs.SetInt("SpielerLevel", int.Parse(levelAnzeige.text));
             SceneManager.LoadScene(level);
         }
     }
