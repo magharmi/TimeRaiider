9dbbb9b069f91854efd806196ef59489d9b796cb
magharmi <39556577+magharmi@users.noreply.github.com>
2019-01-21 20:41:42 +0100

    Allen Waffen mehr Schaden gegeben

--- test/Assets/script/PlayerAttack.cs
+++ test/Assets/script/PlayerAttack.cs
@@ -48,53 +48,65 @@ public class PlayerAttack : MonoBehaviour
                 if (waffennummer == 0)
                 {
                     anim.SetTrigger("isKnochen");
+                    damage = 15;
                 }
                 else if (waffennummer == 1)
                 {
-                    anim.SetTrigger("isKnochen");
+                    anim.SetTrigger("isKnochen1");
+                    damage = 25;
                 }
                 else if (waffennummer == 2)
                 {
                     anim.SetTrigger("isAxtAngriff");
+                    damage = 20;
                 }
                 //Akt2
                 else if (waffennummer == 3)
                 {
                     anim.SetTrigger("isAkt2_Schwert");
+                    damage = 30;
                 }
                 else if (waffennummer == 4)
                 {
                     anim.SetTrigger("isAkt2_Schwert1");
+                    damage = 35;
                 }
                 else if (waffennummer == 5)
                 {
                     anim.SetTrigger("isAkt2_Schwert3");
+                    damage = 40;
                 }
                 //Akt3
                 else if (waffennummer == 6)
                 {
                     anim.SetTrigger("isAkt3_Axt");
+                    damage = 45;
                 }
                 else if (waffennummer == 7)
                 {
                     anim.SetTrigger("isAkt3_Schwert");
+                    damage = 50;
                 }
                 else if (waffennummer == 8)
                 {
                     anim.SetTrigger("isAkt3_Keule");
+                    damage = 55;
                 }
                 //Akt4
                 else if (waffennummer == 9)
                 {
                     anim.SetTrigger("isShoot");
+                    damage = 60;
                 }
                 else if (waffennummer == 10)
                 {
                     anim.SetTrigger("isAkt4_Ak");
+                    damage = 70;
                 }
                 else if (waffennummer == 11)
                 {
                     anim.SetTrigger("isAkt4_Pistol");
+                    damage = 65;
                 }
 
                 if (Input.GetMouseButtonDown(0))
