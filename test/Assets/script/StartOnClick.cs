c30bb91327f74849dab54de7ec255bd08c0d5905
magharmi <39556577+magharmi@users.noreply.github.com>
2019-01-21 13:50:54 +0100

    Neues Spiel startet bei "Feld"

--- test/Assets/script/StartOnClick.cs
+++ test/Assets/script/StartOnClick.cs
@@ -11,7 +11,7 @@ public class StartOnClick : MonoBehaviour {
         string lastScene = PlayerPrefs.GetString("letzteScene");
         if (lastScene == "null")
         {
-            SceneManager.LoadScene("Storybilder");
+            SceneManager.LoadScene("Feld");
         }
         else
         {
